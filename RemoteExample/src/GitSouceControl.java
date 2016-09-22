dcb81c42f776e1c52061e7e5cef1faf8100a642d
Rox <rox.orange@mtsgl.com>
2016-09-22 12:08:31 +1200

    Updated Package

---
+++ RemoteExample/src/GitSouceControl.java
@@ -0,0 +1,11 @@
+
+public class GitSouceControl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("The is my remote example :)");;
+
+	}
+
+}
